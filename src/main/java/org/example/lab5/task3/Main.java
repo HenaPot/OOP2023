9f195352a993720bb147e2266c4bb418618f1a36
Hena Potogija <hena.potogija@stu.ibu.edu.ba>
2023-11-08 13:40:17 +0100

    final touches lab week 5

--- src/main/java/org/example/lab5/task3/Main.java
+++ src/main/java/org/example/lab5/task3/Main.java
@@ -1,4 +1,38 @@
 package org.example.lab5.task3;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
+    public static void main(String[] args) {
+        //upcasting
+        Thing hena = new Thing(2, "Hena Potogija");
+        MaxWeightBox maxBox = new MaxWeightBox(10);
+        OneThingBox oneThingBox = new OneThingBox();
+        BlackHoleBox blackHoleBox = new BlackHoleBox();
+
+        // we can cast these to abstract class variable
+        Box box1 = (Box) hena;
+        Box box2 = (Box) maxBox;
+        Box box3 = (Box) oneThingBox;
+        Box box4 = (Box) blackHoleBox;
+
+        /* the following code does not work because thing is cast to be Box and can
+        only have functions and attributes that are associated with it
+        System.out.println(box1.getWeight);*/
+
+        //downcasting
+        if (box1 instanceof Thing){
+            hena = (Thing) box1;
+        }
+        if (box2 instanceof MaxWeightBox){
+            maxBox = (MaxWeightBox) box2;
+        }
+        if (box3 instanceof OneThingBox){
+            oneThingBox = (OneThingBox) box3;
+        }
+        if (box4 instanceof BlackHoleBox){
+            blackHoleBox = (BlackHoleBox) box4;
+        }
+    }
+
 }
--- src/main/java/org/example/lab5/task3/Thing.java
+++ src/main/java/org/example/lab5/task3/Thing.java
@@ -1,11 +1,16 @@
 package org.example.lab5.task3;
 
-public class Thing {
-    private double weight;
+import java.util.ArrayList;
+import java.util.List;
 
-    public Thing(double weight){
+class Thing extends Box{
+    private double weight;
+    private String name;
+
+    public Thing(double weight, String name){
         if (weight >= 0) {
             this.weight = weight;
+            this.name = name;
         }
         else throw new IllegalArgumentException("This value is negative!");
     }
@@ -16,16 +21,106 @@ public class Thing {
 
     @Override
     public boolean equals(Object obj){
+        boolean isEqual = false;
         // this is downcasting, additional validation is recommended
-        Thing thing = (Thing) obj;
-        if (this.weight == thing.getWeight()){
-            return true;
+        if ( obj != null && obj instanceof Thing){
+            // mozemo koristit  == jer se this.name racuna kao objekat...
+            isEqual = (this.name == ((Thing)obj).name);
         }
-        else {
-            return false;
+        return isEqual;
+    }
+
+    @Override
+    public void add(Thing thing) {
+
+    }
+
+    @Override
+    public boolean isInTheBox(Thing thing) {
+        return false;
+    }
+
+}
+
+class MaxWeightBox extends Box{
+    private int maxWeight;
+    private int currentWeight;
+    private List<Thing> box;
+    public MaxWeightBox(int maxWeight){
+        this.maxWeight = maxWeight;
+        this.currentWeight = 0;
+        this.box = new ArrayList<Thing>();
+    }
+
+    @Override
+    public void add(Thing thing) {
+        if (thing.getWeight() <= (this.maxWeight-this.currentWeight)) {
+            box.add(thing);
+            this.currentWeight += (int) thing.getWeight();
         }
     }
 
-    /*@)Override
-    public int hashCode*/
+    @Override
+    public boolean isInTheBox(Thing thing) {
+        boolean found = false;
+        for (Thing t : box){
+            if (t.equals(thing)){
+                found = true;
+                break;
+            }
+        }
+        return found;
+    }
+}
+
+class OneThingBox extends Box{
+    private int numOfItems;
+    private List<Thing> box;
+    public OneThingBox(){
+        this.numOfItems = 0;
+        this.box = new ArrayList<Thing>();
+    }
+
+    @Override
+    public void add(Thing thing) {
+        if (this.numOfItems == 0){
+            // radi i sa this i bez this ?
+            this.box.add(thing);
+            this.numOfItems = 1;
+        }
+
+    }
+
+    @Override
+    public boolean isInTheBox(Thing thing) {
+        boolean found = false;
+        for (Thing t : box){
+            if (t.equals(thing)){
+                found = true;
+                break;
+            }
+        }
+        return found;
+    }
+
+}
+
+class BlackHoleBox extends Box{
+
+    private List<Thing> box;
+
+    public BlackHoleBox(){
+        this.box = new ArrayList<Thing>();
+    }
+
+    @Override
+    public void add(Thing thing) {
+        box.add(thing);
+
+    }
+
+    @Override
+    public boolean isInTheBox(Thing thing) {
+        return false;
+    }
 }
